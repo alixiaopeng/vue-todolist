9cae30aa5b45244189b414097109e7478cf299a8
zhujiapeng <297071593@qq.com>
2020-06-29 09:28:19 +0800

    项目完成

--- js/index.js
+++ js/index.js
@@ -3,9 +3,15 @@ new Vue({
 	data: {
 		editMaskFlag: false,
 		deleteMaskFlag: false,
+		activeIndex: 0,
 		inputBoxVal: "",
 		todos: [],
-	},
+		newTodos: [],
+		allFlag: true,
+		finishFlag: false,
+		UnFinishFlag: false,
+    },
+    
 	methods: {
 		/**
 		 * 显示添加信息弹出框
@@ -24,8 +30,10 @@ new Vue({
 		/**
 		 * 显示删除信息弹出框
 		 */
-		showDeleteTipsMask() {
+		showDeleteTipsMask(index) {
 			this.deleteMaskFlag = true;
+			//将当前元素索引值赋给activeIndex
+			this.activeIndex = index;
 		},
 
 		/**
@@ -54,11 +62,59 @@ new Vue({
 			this.todos.push({
 				id: this.todos.length + 1,
 				task: this.inputBoxVal,
+				flag: false,
 			});
+			this.newTodos = this.todos;
 			// 将输入框清空
 			this.inputBoxVal = "";
 			// 隐藏添加信息弹出框
 			this.hideEditMask();
+		},
+
+		/**
+		 * 删除信息
+		 */
+		remove(index) {
+			this.todos.splice(index, 1);
+			this.newTodos = this.todos;
+			this.hideDeleteTipsMask();
+		},
+
+		/**
+		 * 改变当前一项的flag，来改变选中按钮的状态
+		 */
+		changeFlag(index) {
+			this.todos[index].flag = !this.todos[index].flag;
+		},
+
+		/**
+		 * 点击所有按钮
+		 */
+		checkedAll() {
+			this.newTodos = this.todos;
+			this.allFlag = true;
+			this.finishFlag = false;
+			this.UnFinishFlag = false;
+		},
+
+		/**
+		 * 点击完成按钮
+		 */
+		checkedFinish() {
+			this.newTodos = this.todos.filter((item) => item.flag);
+			this.allFlag = false;
+			this.finishFlag = true;
+			this.UnFinishFlag = false;
+		},
+
+		/**
+		 * 点击未完成按钮
+		 */
+		checkedUnFinish() {
+			this.newTodos = this.todos.filter((item) => !item.flag);
+			this.allFlag = false;
+			this.finishFlag = false;
+			this.UnFinishFlag = true;
 		},
 	},
 });
